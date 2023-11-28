3251166995dc0596d986625d64e174a086a31824
Yanka Domenikova <yankadomenikova@gmail.com>
2023-11-28 16:23:49 +0200

    Add Footer component and css module for the Footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 import "./App.module.css";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header />
+
     </>
   )
 }
