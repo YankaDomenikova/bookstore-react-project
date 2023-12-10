7345387eccfdb3987b22177f8b65fd8852b1af7d
Yanka Domenikova <yankadomenikova@gmail.com>
2023-12-10 17:41:13 +0200

    Add banner to Home component

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,12 +1,18 @@
+import { Link } from 'react-router-dom';
+import { Paths } from '../../paths/paths';
 import styles from './Home.module.css'
 
 export default function Home() {
     return (
-        <div>
-            <div className={styles.container}>
-                <h1>Home Page</h1>
-                <button>Shop now</button>
-            </div>
-        </div>
+        <>
+            <div className={styles.banner}>
+                <img src="books-cover-2-removebg-preview.png" alt="" />
+                <div class={styles.bannerText}>
+                    <h1>Books for everyone</h1>
+                    <Link to={Paths.Catalog}>Shop now</Link>
+                </div>
+                <img src="books-cover-3-removebg-preview.png" alt="" />
+            </div >
+        </>
     );
 }
