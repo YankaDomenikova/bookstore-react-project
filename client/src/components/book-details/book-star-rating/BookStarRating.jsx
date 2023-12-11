9b6c033af2f31c9be1ece36478089b7cfcd7ed60
Yanka Domenikova <yankadomenikova@gmail.com>
2023-12-11 14:24:52 +0200

    Add total star rating component

--- client/src/components/book-details/book-star-rating/BookStarRating.jsx
+++ client/src/components/book-details/book-star-rating/BookStarRating.jsx
@@ -1,20 +1,14 @@
 import styles from './BookStarRating.module.css'
 
 export default function BookStarRating({ value }) {
-    /* Calculate how much of the stars should be "filled" */
     const percentage = Math.round((value / 5) * 100);
 
     return (
         <div className={styles.container}>
-            {
-                /* Create an array based on the max rating, render a star for each */
-            }
             {Array.from(Array(5).keys()).map((_, i) => (
                 <span key={i} className={styles.star}>&#9733;</span>
             ))}
-            {
-                /* Render a div overlayed on top of the stars that should not be not filled */
-            }
+
             <div className={styles.overlay} style={{ width: `${100 - percentage}%` }} />
         </div>
     );
