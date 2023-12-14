93bc2b8668a3ae40e4c449cb96c8cc23aa78c76c
Yanka Domenikova <yankadomenikova@gmail.com>
2023-12-14 22:44:52 +0200

    Add order services Fetch and display all user orders

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,35 +1,53 @@
+import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import * as ordeerService from '../../services/orderService';
+import { convert } from '../../utils/dateConverter';
+
 import styles from './Profile.module.css'
 import arrowRight from '../../assets/arrow-right-svgrepo-com.svg';
+import AuthContext from '../../contexts/AuthContext';
 
 export default function Profile() {
+    const [orders, setOrders] = useState([]);
+    const { userId, username, email } = useContext(AuthContext);
+
+    useEffect(() => {
+        ordeerService.getAll(userId)
+            .then(result => setOrders(result));
+    }, []);
+
+
     return (
         <div className={styles.pageContent}>
-            <div class={styles.authinfo}>
-                <h3>Username</h3>
-                <p>mail@mail.com</p>
+            <div className={styles.authinfo}>
+                <h3>{username}</h3>
+                <p>{email}</p>
             </div>
 
-            <div class={styles.nav}>
-                <h5 class={styles.navItem}>My orders</h5>
+            <div className={styles.nav}>
+                <h5 className={styles.navItem}>My orders</h5>
             </div>
 
             <div className={styles.ordersContainer}>
-                <div class={styles.order}>
-                    <div class={styles.orderOverview}>
-                        <p class={styles.orderNumber}>99654821</p>
-                        <div>
-                            <p>$ 55.05</p>
-                            <p>18 June 2023</p>
-                        </div>
-                    </div>
-                    <Link class={styles.arrow}>
-                        <img src={arrowRight} alt="" />
-                    </Link>
-                </div>
+                {orders.length === 0 && <h3 className={styles.noOrdersText}>No orders yet</h3>}
 
+                {orders.map(order => (
+                    <div className={styles.order} key={order._id}>
+                        <div className={styles.orderOverview}>
+                            <p className={styles.orderNumber}>{order.orderNumber}</p>
+                            <div>
+                                <p>$ {order.totalPrice}</p>
+                                <p>{convert(order._createdOn)}</p>
+                            </div>
+                        </div>
+                        <Link className={styles.arrow}>
+                            <img src={arrowRight} alt="" />
+                        </Link>
+                    </div>
+                ))}
 
             </div>
-        </div>
+        </div >
     );
 }
--- client/src/services/orderService.js
+++ client/src/services/orderService.js
@@ -2,6 +2,16 @@ import { request } from '../lib/request';
 
 const baseUrl = "http://localhost:3030/data/orders";
 
+export const getAll = async (userId) => {
+    const query = new URLSearchParams({
+        where: `_ownerId="${userId}"`,
+    });
+
+    const result =  await request('GET', `${baseUrl}?${query}`);
+    return result;
+};
+
+
 export const create = async (email,
     fullname,
     phoneNumber,
