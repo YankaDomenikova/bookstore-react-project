854258dcd47b8c9486324f23483e368929831b5e
Yanka Domenikova <yankadomenikova@gmail.com>
2023-12-16 23:02:40 +0200

    Reset basket when user logs out

--- client/src/components/authentication/logout/Logout.jsx
+++ client/src/components/authentication/logout/Logout.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import AuthContext from "../../../contexts/AuthContext";
+import ShoppingContext from "../../../contexts/ShoppingContext";
 import * as authService from '../../../services/authService';
 import { Paths } from "../../../paths/paths";
 
@@ -9,16 +10,19 @@ export default function Logout() {
     const navigate = useNavigate();
 
     const { logoutHandler } = useContext(AuthContext);
+    const { resetBasket } = useContext(ShoppingContext);
 
     useEffect(() => {
         authService.logout()
             .then(() => {
                 logoutHandler();
-                navigate(Paths.Home);;
+                resetBasket()
+                navigate(Paths.Home);
             })
             .catch(() => {
                 logoutHandler();
-                navigate(Paths.Login);;
+                resetBasket();
+                navigate(Paths.Login);
             });
     }, [])
 
